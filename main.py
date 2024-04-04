8ff92d64beec11b4f7700df2cea2db15417351e1
MiMoHD01 <134415019+MiMoHD01@users.noreply.github.com>
2024-04-04 21:53:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.clashchamps.com/looking-for-players-clan-sign-up/")
 print(driver.title)
 wait = WebDriverWait(driver, 10)
-sign_in = "(/html/body/div[1]/div/div[4]/div[2]/div/div[1]/div/article/div/div/div/section/div/div[1]/div/div/div[5]/div/div/span/a)[1]"
+sign_in = "(/html/body/div[1]/div/div[4]/div[2]/div/div[1]/div/article/div/div/div/section/div/div[1]/div/div/div[5]/div/div/span/a[contains(text(sign in))"
 wait.until(EC.element_to_be_clickable(By.XPATH, sign_in))
 driver.find_element(By.XPATH, sign_in).click
 WebDriverWait(driver, 10)
