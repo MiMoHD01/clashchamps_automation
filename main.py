0328c0306cc9e7f887aeda7c811fe5d4fcb05f68
MiMoHD01 <134415019+MiMoHD01@users.noreply.github.com>
2024-04-25 20:01:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from seleniumbase import SB
 
-
-WINDOW_SIZE = "1920,1080"
-
-# Options
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(options=chrome_options)
-
-# Get the response and print title
-driver.get("https://www.clashchamps.com/looking-for-players-clan-sign-up/")
-print(driver.title)
-wait = WebDriverWait(driver, 10)
-sign_in = "(/html/body/div[1]/div/div[4]/div[2]/div/div[1]/div/article/div/div/div/section/div/div[1]/div/div/div[5]/div/div/span/a[contains(text(sign in))"
-wait.until(EC.element_to_be_clickable(By.XPATH, sign_in))
-driver.find_element(By.XPATH, sign_in).click
-WebDriverWait(driver, 10)
-print(driver.title)
-driver.close()
+with SB(uc=True, test=True, locale_code="en") as sb:
+    url = "https://ahrefs.com/website-authority-checker"
+    input_field = 'input[placeholder="Enter domain"]'
+    submit_button = 'span:contains("Check Authority")'
+    sb.driver.uc_open_with_reconnect(url, 1)  # The bot-check is later
+    sb.type(input_field, "github.com/seleniumbase/SeleniumBase")
+    sb.driver.reconnect(0.1)
+    sb.driver.uc_click(submit_button, reconnect_time=4)
+    sb.wait_for_text_not_visible("Checking", timeout=10)
+    sb.highlight('p:contains("github.com/seleniumbase/SeleniumBase")')
+    sb.highlight('a:contains("Top 100 backlinks")')
+    sb.set_messenger_theme(location="bottom_center")
+    sb.post_message("SeleniumBase wasn't detected!")
