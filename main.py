8e8fea8772f15483c8d2bea020a8d785be810ced
MiMoHD01 <134415019+MiMoHD01@users.noreply.github.com>
2024-04-29 18:25:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from seleniumbase import SB
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-with SB(uc=True, test=True, locale_code="en") as sb:
-    url = "https://ahrefs.com/website-authority-checker"
-    input_field = 'input[placeholder="Enter domain"]'
-    submit_button = 'span:contains("Check Authority")'
-    sb.driver.uc_open_with_reconnect(url, 1)  # The bot-check is later
-    sb.type(input_field, "github.com/seleniumbase/SeleniumBase")
-    sb.driver.reconnect(0.1)
-    sb.driver.uc_click(submit_button, reconnect_time=4)
-    sb.wait_for_text_not_visible("Checking", timeout=10)
-    sb.highlight('p:contains("github.com/seleniumbase/SeleniumBase")')
-    sb.highlight('a:contains("Top 100 backlinks")')
-    sb.set_messenger_theme(location="bottom_center")
-    sb.post_message("SeleniumBase wasn't detected!")
+# Set path Selenium
+CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
+s = Service(CHROMEDRIVER_PATH)
+WINDOW_SIZE = "1920,1080"
+
+# Options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(service=s, options=chrome_options)
+
+# Get the response and print title
+driver.get("https://www.python.org")
+print(driver.title)
+driver.close()
